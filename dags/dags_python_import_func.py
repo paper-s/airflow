04d99965dc80c5bf3262b1d7491a404a9027cfe8
alti0820 <alti0820@kakao.com>
2025-06-22 17:15:08 +0900

    공통 외부함수 추가

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -1,25 +1,18 @@
 
 from airflow import DAG
-import datetime
 import pendulum
+import datetime
 from airflow.operators.python import PythonOperator
-import random
-
+from common.common_func import get_sftp
 
 with DAG(
-    dag_id="dags_python_operator",
-    schedule="30 6 * * *",
-    start_date=pendulum.datetime(2025, 6, 22, tz="Asia/Seoul"),
+    dag_id="dags_python_import_func"
+    schedule="30 6 * * *"
+    start_date=pendulum.datetime(2025, 6, 21, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    def select_fruit():
-        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0,3)
-        print(fruit[rand_int])
-    
-    py_t1 = PythonOperator(
-        task_id = 'py_t1',
-        python_callable=select_fruit
-    )
 
-    py_t1
+    task_get_sftp = PythonOperator(
+        task_id = 'task_get_sftp',
+        python_callable=get_sftp
+    )
