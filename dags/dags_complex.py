76b75221df7387f1d5128089eeffd2fd85d051c0
jslee <alti0820@kakao.com>
2025-06-28 16:08:29 +0900

    complex dags 업로드

---
+++ dags/dags_complex.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+
+
+with DAG(
+    dag_id="dags_complex",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2025, 6, 28, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_t1 = BashOperator(
+        task_id = 'bash_t1',
+        bash_command = 'echo whoami',
+    )
+
+    bash_t2 = BashOperator(
+        task_id = 'bash_t2',
+        bash_command = 'echo $HOSTNAME',
+    )
+
+    bash_t1 >> bash_t2
